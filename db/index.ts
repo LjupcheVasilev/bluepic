1e47077c85288fb11077edb8bd96247b786c37f9
Ljupche Vasilev <ljupche.vasilev@gmail.com>
2025-02-12 21:10:09 +0000

    fix: supabase connection

--- db/index.ts
+++ db/index.ts
@@ -2,7 +2,10 @@ import { drizzle } from 'drizzle-orm/postgres-js'
 import postgres from 'postgres'
 
 const connectionString = process.env.DATABASE_URL!
-const client = postgres(connectionString)
-export const db = drizzle(client);
+const client = postgres(connectionString, {
+    ssl: { rejectUnauthorized: false },
+    prepare: false
+})
+export const db = drizzle(client)
 
 export type Database = typeof db
