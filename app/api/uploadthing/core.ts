a80472d82cd74d555ebd75a8385c9b498e847808
Ljupche Vasilev <ljupche.vasilev@gmail.com>
2025-02-17 12:15:28 +0000

    feat: change uploadthing max file size

--- app/api/uploadthing/core.ts
+++ app/api/uploadthing/core.ts
@@ -1,17 +1,17 @@
-import { createUploadthing, type FileRouter } from "uploadthing/next";
+import { createUploadthing, type FileRouter } from "uploadthing/next"
 // import { UploadThingError } from "uploadthing/server";
 
-const f = createUploadthing();
+const f = createUploadthing()
 
 export const ourFileRouter = {
-  imageUploader: f({ image: { maxFileSize: "4MB", maxFileCount: 1 } })
+  imageUploader: f({ image: { maxFileSize: "16MB", maxFileCount: 1 } })
     .middleware(() => {
       // Static middleware that doesn't depend on request
-      return { userId: "user_123" };
+      return { userId: "user_123" }
     })
     .onUploadComplete(async ({ metadata, file }) => {
-      return { uploadedBy: metadata.userId };
+      return { uploadedBy: metadata.userId }
     }),
-} satisfies FileRouter;
+} satisfies FileRouter
 
-export type OurFileRouter = typeof ourFileRouter;
+export type OurFileRouter = typeof ourFileRouter
